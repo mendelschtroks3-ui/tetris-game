b24f9a835be6c2d8881264deb495521bb326a3de
mendelschtroks3-ui <mendelschtroks3@gmail.com>
2026-02-13 15:59:05 -0800

    Create pages/index.js with basic Tetris component

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tetris = () => {
+    return (
+        <div>
+            <h1>Tetris Game</h1>
+            <p>This is the Tetris game component!</p>
+        </div>
+    );
+};
+
+export default Tetris;
